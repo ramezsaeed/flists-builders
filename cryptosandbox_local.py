c513200ed618dd8d17e2ffdc65a5a1f204f2d0ed
Ahmed T. Youssef <xmonader@gmail.com>
2018-05-17 12:39:11 +0200

    update cryptosanbox

--- cryptosandbox_local.py
+++ cryptosandbox_local.py
@@ -29,9 +29,19 @@ def do_pkgsandbox_and_push(prefabpkg, flistname="cryptosandbox", bins=None):
     prefab.core.execute_bash(
         '''curl -b 'caddyoauth={}' -F file=@/tmp/{}.tar.gz https://hub.gig.tech/api/flist/me/upload'''.format(iyo_client.jwt, flistname)
     )
-
+    prefab.core.execute_bash(
+        '''curl -b 'caddyoauth={}' -F file=@/tmp/{}.tar.gz http://192.168.20.132:8080/api/flist/me/upload'''.format(iyo_client.jwt, flistname)
+    )
     return "{}/{}.flist".format(username, flistname)
 
+
+def merge(client, target, sources):
+    url = '{}/flist/me/merge/{}'.format(client.api.base_url, target)
+    resp = client.api.post(uri=url, data=json.dumps(sources), headers=None, params=None, content_type='application/json')
+    print(resp)
+    return "{}/{}".format(username, target)
+
+    
 if __name__ == "__main__":
     prefab = j.tools.prefab.local 
     bitcoinbins=["/opt/bin/bitcoind", "/opt/bin/bitcoin-cli", "/opt/bin/bitcoin-tx"]
@@ -43,14 +53,26 @@ if __name__ == "__main__":
     tfchainflist = do_pkgsandbox_and_push(prefab.blockchain.tfchain, flistname="tfchainflist", bins=tfchainbins)
     ethereumflist = do_pkgsandbox_and_push(prefab.blockchain.ethereum, flistname="ethereumflist", bins=ethbins)
 
-    sources = [btcflist, tfchainflist, ethereumflist]
-    target = 'cryptosandbox.flist'
+    cryptosources = [btcflist, tfchainflist, ethereumflist]
+    cryptosandboxtarget = 'cryptosandbox.flist'
     
     zhub_data = {'token_': iyo_client.jwt, 'username': 'thabet','url': 'https://hub.gig.tech/api'}
-    zhub_client = j.clients.zhub.get(instance="main", data=zhub_data)
+    zhub_client = j.clients.zhub.get(instance="mainbe", data=zhub_data)
     zhub_client.authentificate()
-    
-    url = '{}/flist/me/merge/{}'.format(zhub_client.api.base_url, target)
-    resp = zhub_client.api.post(uri=url, data=json.dumps(sources), headers=None, params=None, content_type='application/json')
-    print(resp)
-    print("https://hub.gig.tech/{}/{}".format(username, target))
+
+
+    cryptoflist= merge(zhub_client, cryptosandboxtarget, cryptosources)
+    ubuntucryptotarget = "ubuntucrypto.flist"
+    ubuntucryptosources = [btcflist, tfchainflist, ethereumflist, "azmy/ubuntu-xenial-bootable-sshd.flist"]
+
+    ubuntucryptoflist = merge(zhub_client, ubuntucryptotarget, ubuntucryptosources)
+
+    print("https://hub.gig.tech/{}".format(ubuntucryptoflist))
+
+    # try to do localhub
+    zhub_dataeg = {'token_': iyo_client.jwt, 'username': 'thabet','url': 'http://192.168.20.132:8080/api'}
+    zhub_clienteg = j.clients.zhub.get(instance="maineg", data=zhub_dataeg)
+    zhub_clienteg.authentificate()
+
+    merge(zhub_clienteg, cryptosandboxtarget, cryptosources)
+    merge(zhub_clienteg, ubuntucryptotarget, ubuntucryptosources)
